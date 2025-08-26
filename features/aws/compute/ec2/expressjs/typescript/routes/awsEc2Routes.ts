2a8ebe0a566982d163a23ac65e5b734c2043bfcf
Shariq Choudhary <khanshariq92213@gmail.com>
2025-08-26 18:07:01 +0530

    AWS Elastic Load Balancer Service Added as feature for nestjs,nextjs,expressjs and remixjs

--- features/aws/compute/ec2/expressjs/typescript/routes/awsEc2Routes.ts
+++ features/aws/compute/ec2/expressjs/typescript/routes/awsEc2Routes.ts
@@ -4,6 +4,6 @@ import { getInstances, manageInstance } from "../controllers/awsEc2Controller";
 const router = Router();
 
 router.get("/instances", getInstances);
-router.post("/instance", manageInstance);
+router.post("/manage", manageInstance);
 
 export default router;
