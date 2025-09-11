935de4f8013aec1b97ef0d8f87ebc8f4dccd331f
Shariq Choudhary <khanshariq92213@gmail.com>
2025-09-11 22:38:51 +0530

    Minor Changes

--- features/database/postgres/drizzle/typescript/expressjs/utils/drizzle.ts
+++ features/database/postgres/drizzle/typescript/expressjs/utils/drizzle.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { Pool } from "mysql2";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
