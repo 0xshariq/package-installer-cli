2ce8667ec4a8b2f1692e880b52a30c561e3217f7
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-10-21 09:02:32 +0000

    bottle code added

--- templates/python/bottle/template/main.py
+++ templates/python/bottle/template/main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from fastapi!")
+from bottle import route, run, response  # type: ignore[import]
 
+@route('/')
+def home():
+    response.content_type = 'application/json'
+    return {'message': 'Hello from Bottle!'}
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    run(host='localhost', port=8080, debug=True)
